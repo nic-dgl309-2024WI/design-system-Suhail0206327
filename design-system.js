3636ddb8c82d9ac6406b65f29820825fd38ee77a
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2024-02-08 10:11:16 -0800

    Define a function to toggle the menu

---
+++ design-system.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function toggleButton(){
+
+    let navbar = document.getElementById("navbar");
+    let sideNavbar = document.getElementById("side-navigation");
+
+    if(navbar.className ==='c-menu') {
+        navbar.className === 'menu__mobileNavbar';
+        sidenavbar.className === "menu__mobileSideNavbar";
+    }
+}
