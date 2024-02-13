7cf9ea8c3a07741f4a732dedaca5864b556400ce
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2024-02-12 22:03:23 -0800

    Create color card

--- design-system.js
+++ design-system.js
@@ -4,13 +4,17 @@ function toggleButton(){
 
     let navbar = document.getElementById("navbar");
     let sideNavbar = document.getElementById("side-navigation");
+    let content =document.getElementById("content");
 
     if(navbar.className ==='c-menu') {
         navbar.className = 'menu__mobileNavbar';
         sideNavbar.className = "menu__mobileSideNavbar";
+        content.style.marginTop= 50;
     }
     else {
         navbar.className ='c-menu';
         sideNavbar.className ='c-menu__sidenavigation';
+        content.style.marginTop= 50;
     }
 }
+
