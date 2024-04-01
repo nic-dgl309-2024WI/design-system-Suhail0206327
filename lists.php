6ed5dede7334b4bbd3b42fd0223e46ad4b9c1a0c
Suhail0206327 <122574657+Suhail0206327@users.noreply.github.com>
2024-04-01 07:05:00 -0700

    Create the list section

--- lists.php
+++ lists.php
@@ -3,19 +3,32 @@
 require ('functions.php');
 
 require('header.php'); ?>
-<main>
-<h1>
+<main  class="l-p-5" >
+<h1 class="l-mb-4" >
     Lists
 </h1>
 
-<section>
-    <ul class="c-list">
-        <li class="c-list__item" >item 1</li>
-        <li class="c-list__item" >item 2</li>
-        <li class="c-list__item" >item 3</li>
-        <li class="c-list__item" >item 4</li>
-        <li class="c-list__item" >item 5</li>
+<section class="c-list-section " >
+    <ul class="c-list l-ml-1">
+        <li class="c-list__item l-p-1" >A list item 1</li>
+        <li class="c-list__item l-p-1" >A list item 1</li>
+        <li class="c-list__item l-p-1" >A list item 1</li>
+        <li class="c-list__item l-p-1" >A list item 1</li>
+        <li class="c-list__item l-p-1" >A list item 1</li>
     </ul>
+    <div class="c-code ">
+        <p class="u-fs-small l-p-1">HTML</p>
+        <ul class="l-p-1 l-pl-5" >
+        <li>&lt;ul class="c-list"&gt;</li>
+    <li class="l-pl-1" >&lt;li class="c-list__item l-p-1"&gt;A list item &lt;/li&gt;</li>
+    <li class="l-pl-1" >&lt;li class="c-list__item l-p-1"&gt;A list item 1&lt;/li&gt;</li>
+    <li class="l-pl-1" >&lt;li class="c-list__item l-p-1"&gt;A list item 1&lt;/li&gt;</li>
+    <li class="l-pl-1" >&lt;li class="c-list__item l-p-1"&gt;A list item 1&lt;/li&gt;</li>
+    <li class="l-pl-1" >&lt;li class="c-list__item l-p-1"&gt;A list item 1&lt;/li&gt;</li>
+    <li  >&lt;/ul&gt;</li>
+        </ul>
+   
+    </div>
 </section>
 
 </main> 
